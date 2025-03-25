80ce6689959bcb9b253c6777c4e759a7b6718486
LuisRicardo <palharini.luis@gmail.com>
2025-03-25 17:42:44 -0300

    feat: Extract playlist data from Spotify

--- app/main.py
+++ app/main.py
@@ -1,37 +1,21 @@
 from dotenv import load_dotenv
 from pathlib import Path
-import os
 
-import requests
+# Token
+from utlis.spotify_token import generate_access_token
+
+# ETL imports
+import ETL.extract
 
 dotenv_path = Path('./.env')
 load_dotenv(dotenv_path=dotenv_path)
 
-def generate_access_token() -> str:
-    """
-    Function to generate the Spotify's access token
-
-    Returns:
-        str: The access token.
-    """
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    data = {
-        "grant_type": "client_credentials",
-        "client_id": str(os.getenv("SPOTIFY_CLIENT_ID")),
-        "client_secret": str(os.getenv("SPOTIFY_CLIENT_SECRET"))
-    }
-
-    response = requests.post(url="https://accounts.spotify.com/api/token", headers=headers, data=data)
-
-    if response.status_code != 200:
-        raise Exception("Error while generating the access token.")
-    
-    access_token = response.json().get('access_token')
-    if not access_token:
-        raise Exception("Error while generating the access token: access_token missing.")
-    
-    return access_token
-
 if __name__ == "__main__":
+    playlist_id = "5FN6Ego7eLX6zHuCMovIR2"
     access_token = generate_access_token()
     print("Access token generated!")
+
+    ETL.extract.playlist(
+        playlist_id=playlist_id,
+        access_token=access_token
+    )
