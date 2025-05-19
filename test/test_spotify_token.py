213ecb278305659eca9df6c7665d255d61fb1d20
LuisRicardo <palharini.luis@gmail.com>
2025-05-19 20:37:32 -0300

    refactor: Changing Token function

--- test/test_spotify_token.py
+++ test/test_spotify_token.py
@@ -6,21 +6,21 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pytest
 from unittest.mock import MagicMock
 
-from app.utlis.spotify_token import generate_access_token 
+from app.utlis.spotify import token 
 
-def test_generate_access_token_success(mocker):
+def test_token_success(mocker):
     mock_response = MagicMock()
     mock_response.status_code = 200
     mock_response.json.return_value = {'access_token': 'valid-token'}
     
     mocker.patch("requests.post", return_value=mock_response)
     
-    result = generate_access_token()
+    result = token()
     
     assert result == 'valid-token'
 
 
-def test_generate_access_token_error(mocker):
+def test_token_error(mocker):
     mock_response = MagicMock()
     mock_response.status_code = 400
     mock_response.text = "Invalid request"
@@ -28,14 +28,14 @@ def test_generate_access_token_error(mocker):
     mocker.patch("requests.post", return_value=mock_response)
     
     with pytest.raises(Exception, match="Error while generating the access token."):
-        generate_access_token()
+        token()
 
 
 def test_missing_client_id_or_secret(mocker):
     mocker.patch("os.getenv", return_value=None)
     
     with pytest.raises(Exception, match="Error while generating the access token."):
-        generate_access_token()
+        token()
 
 
 def test_missing_access_token_in_response(mocker):
@@ -46,4 +46,4 @@ def test_missing_access_token_in_response(mocker):
     mocker.patch("requests.post", return_value=mock_response)
     
     with pytest.raises(Exception, match="Error while generating the access token."):
-        generate_access_token()
+        token()
